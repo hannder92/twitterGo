3aec8bed6e3d84ab465cc43ad4aaf918f43d4c1c
Johann Medina <hannder92@gmail.com>
2024-05-15 18:29:09 -0500

    Ultimo commit

--- subo.bat
+++ subo.bat
@@ -2,7 +2,7 @@ git add .
 git commit -m "Ultimo commit"
 git push
 set GOOS=linux
-set GOARCH=amd64
+set GOARCH=arm64
 set CGO_ENABLED=0
 go build -tags lambda.norpc -o bootstrap main.go
-%USERPROFILE%\Go\bin\linux_amd64\build-lambda-zip.exe -o myFunction.zip bootstrap
+%USERPROFILE%\Go\bin\build-lambda-zip.exe -o myFunction.zip bootstrap
